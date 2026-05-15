4b7bd0c27d4153153b8f58cca071fa04ae59c26f
Thuy3110 <thuydreamcatecher@gmail.com>
2026-05-15 23:28:58 +0700

    fix back-end CI

--- backend/apps/dashboard/urls.py
+++ backend/apps/dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AdminDashboardView,TrainerDashboardView,  MemberDashboardView
+from .views import AdminDashboardView,TrainerDashboardView,MemberDashboardView
 
 urlpatterns = [
     path("admin/",AdminDashboardView.as_view()
